193cdfeb796b0f3965aa7bba888e00f8f5148827
amreemaru <heartof.effort@gmail.com>
2022-06-29 11:00:25 +0700

    add

---
+++ while_loop.py
@@ -0,0 +1,26 @@
+
+# while loop
+from tkinter import N
+
+
+def sum_input():
+    n = int(input("Enter number: "))
+    total = 0
+    while n != 0:
+        total += n
+        n = int(input("Enter number: "))
+    return total
+
+# repeat until know as do .... while
+def sum_input2():
+    total = 0
+    while True: 
+        n = int(input("Enter number: "))
+        if n != 0:
+            total += n
+        else:   
+            break
+    return total
+
+# print("total =", sum_input())
+print("total =", sum_input2())
